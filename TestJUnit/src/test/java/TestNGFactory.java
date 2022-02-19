73b7a657fa6298ace489c6ad4298fc0f804e9846
Olga Zaremskaya <olga_zaremskaya@mail.ru>
2022-02-19 19:16:24 +0700

    Практическое задание 8

--- TestJUnit/src/test/java/TestNGFactory.java
+++ TestJUnit/src/test/java/TestNGFactory.java
@@ -1,4 +1,6 @@
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.testng.Assert;
 import org.testng.annotations.*;
 
@@ -9,15 +11,18 @@ public class TestNGFactory {
 		return new Object [] {
 				new FirstNameTest(), 
 				new LastNameTest(), 
-				new AgeTest()};
+				new AgeTest(),
+				new AgeAvgTest(),
+				new MinAgeTest(),
+				new MaxAgeTest(),
+				new GetFullNameTest()};
 	}
-	
 	}
 class FirstNameTest {
 @DataProvider (name="firstNamesProvider", parallel=true)
 public Object[][] firstNamesProvider () {
 	return new Object[][] {
-		{"Nicola", new Student("nicola","Petrow-Ivanov", 18)},
+		{"N", new Student("n","Petrow-Ivanov", 18)},
 		{"Ivan", new Student("iVan", "gudKOv", 45)},
 		{"Maria-Antuanetta", new Student("Maria-Antuanetta", "sidorova", 50)}
 	};
@@ -26,7 +31,7 @@ public Object[][] firstNamesProvider () {
 	public void firstName(String name, Student a) {
 		Assert.assertEquals(name, a.getFirstName());
 		long id = Thread.currentThread().getId();
-		System.out.println("id тест-метода1: " + id);
+		System.out.println("id тест-метода 1: " + id);
 	}
 }
 
@@ -35,8 +40,8 @@ class LastNameTest {
 	@DataProvider (name="lastNameTest", parallel=true)
 	public Object[][] lastNameTest () {
 		return new Object[][] {
-			{"Petrow-Ivanov", new Student("nicola","Petrow-Ivanov", 18)},
-			{"Gudkov",new Student("iVan", "gudKOv", 45)},
+			{"Petrow-Ivanov", new Student("nicola","Petrow-Ivanov", 10)},
+			{"G",new Student("iVan", "g", 45)},
 			{"Sidorova", new Student("Maria-Antuanetta", "sidorova", 50)}
 		};
 	}
@@ -44,7 +49,7 @@ class LastNameTest {
 		public void lastName(String surname, Student b) {
 			Assert.assertEquals(surname, b.getLastName());
 			long id = Thread.currentThread().getId();
-			System.out.println("id тест-метода2: " + id);
+			System.out.println("id тест-метода 2: " + id);
 		}
 	
 }
@@ -53,7 +58,7 @@ class AgeTest {
 	@DataProvider (name="ageTest", parallel=true)
 	public Object[][] ageTest () {
 		return new Object[][] {
-			{18, new Student("nicola","Petrow-Ivanov", 18)},
+			{18, new Student("nicola","Petrow-Ivanov", 10)},
 			{45, new Student("iVan", "gudKOv", 45)},
 			{50, new Student("Maria-Antuanetta", "sidorova", 50)}
 		};
@@ -62,7 +67,73 @@ class AgeTest {
 		public void age(int  age, Student c) {
 			Assert.assertEquals(age, c.getAge());
 			long id = Thread.currentThread().getId();
-			System.out.println("id тест-метода3: " + id);
+			System.out.println("id тест-метода 3: " + id);
 		}
+}
+class AgeAvgTest {
+	@DataProvider (name="ageAvgTest", parallel=true)
+	public Object[][] ageAvgTest () {
+		return new Object[][] {
+            {26, new Student[] { new Student("Sergey", "Nasedkin", 20), new Student("Ivan", "Nesterov", 40),  new Student("Gleb", "Petrov", 20) }}
+    };
+	}
+				
+	@Test(dataProvider = "ageAvgTest")
+    public void avgAgeTest(int expected, Student[] students) {
+      assertEquals(expected, Student.avgAge(students));
+      long id = Thread.currentThread().getId();
+		System.out.println("id тест-метода 4: " + id);
+    }
+}
+	class MinAgeTest {
+		@DataProvider (name="minAgeTest", parallel=true)
+		public Object[][] minAgeTest () {
+			return new Object[][] {
+	            {18, new Student[] { new Student("Sergey", "Nasedkin", 10), new Student("Ivan", "Nesterov", 40),  new Student("Gleb", "Petrov", 50) }}
+	    };
+		}
+					
+		@Test(dataProvider = "minAgeTest")
+	    public void minAgeTest(int expected, Student[] students) {
+	      assertEquals(expected, Student.minAge(students));
+	      long id = Thread.currentThread().getId();
+			System.out.println("id тест-метода 5: " + id);
+	    }
 	
 }
+	class MaxAgeTest {
+		@DataProvider (name="maxAgeTest", parallel=true)
+		public Object[][] maxAgeTest () {
+			return new Object[][] {
+	            {40, new Student[] { new Student("Sergey", "Nasedkin", 10), new Student("Ivan", "Nesterov", 40),  new Student("Gleb", "Petrov", 50) }}
+	    };
+		}
+					
+		@Test(dataProvider = "maxAgeTest")
+	    public void maxAgeTest(int expected, Student[] students) {
+	      assertEquals(expected, Student.maxAge(students));
+	      long id = Thread.currentThread().getId();
+			System.out.println("id тест-метода 6: " + id);
+	    }
+	}
+	class GetFullNameTest {
+
+		@DataProvider (name="getFulltNameTest", parallel=true)
+		public Object[][] getFulltName () {
+			return new Object[][] {
+				{"Nicola Ivanov", new Student("nicola","Ivanov", 10)},
+				{"Ivan Grib",new Student("iVan", "Grib", 45)},
+				{"Maria Sidorova", new Student("Maria", "sidorova", 50)}
+			};
+		}
+			@Test (dataProvider="getFulltNameTest", threadPoolSize=5)
+			public void getFulltName(String fullname, Student b) {
+				Assert.assertEquals(fullname, b.getFullName());
+				long id = Thread.currentThread().getId();
+				System.out.println("id тест-метода 7: " + id);
+			}
+		
+	}
+
+
+
